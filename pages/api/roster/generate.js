50b09844b80646d8ee8905ecc8952cb6434e7692
AboGhassan2 <soom.mysmail@gmail.com>
2025-10-06 09:24:14 +0300

    Update generate.js

--- pages/api/roster/generate.js
+++ pages/api/roster/generate.js
@@ -1,5 +1,5 @@
 // pages/api/roster/generate.js
-import prisma from '../../../lib/db';
+import { prisma } from '../../../lib/db';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
