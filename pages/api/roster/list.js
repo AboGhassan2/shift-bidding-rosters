a556e8bf77e1d9629c2985aed378e1cdc3223258
AboGhassan2 <soom.mysmail@gmail.com>
2025-10-06 09:23:48 +0300

    Update list.js

--- pages/api/roster/list.js
+++ pages/api/roster/list.js
@@ -1,5 +1,5 @@
 // pages/api/roster/list.js
-import prisma from '../../../lib/db';
+import { prisma } from '../../../lib/db';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
